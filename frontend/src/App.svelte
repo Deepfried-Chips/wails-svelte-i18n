bbf5d621dfafcc2af98a5c67b976a1b5d3d9530d
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2022-08-12 18:49:21 +0300

    initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { addMessages, init, getLocaleFromNavigator, _ } from 'svelte-i18n';
+  import en from './i18n/en.json';
+
+
+  addMessages('en', en);
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator(),
+  });
+  import Home from './pages/home.svelte'
+</script>
+
+<Home/>
